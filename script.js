101c487eef61e264c2b12184314614a164ed0fa1
Gurudev Singh <gurudev620.gs@gmail.com>
2022-05-07 19:35:13 +0530

    updated

--- script.js
+++
@@ -1,7 +0,0 @@
-function openSignup()
-{
-    document.getElementsByClassName('first').style.display='none';
-    document.getElementsByClassName('up').style.display='contents';
-    document.getElementsByClassName('in').style.display='none';
-    document.getElementsByClassName('second').style.display='contents';
-}
